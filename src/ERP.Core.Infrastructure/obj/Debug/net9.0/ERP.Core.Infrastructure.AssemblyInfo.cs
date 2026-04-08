b40f3f83be4c186933d9e56f64a3ca11c5856164
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-04-08 14:48:37 -0600

    feat: agregar exception middleware

--- src/ERP.Core.Infrastructure/obj/Debug/net9.0/ERP.Core.Infrastructure.AssemblyInfo.cs
+++ src/ERP.Core.Infrastructure/obj/Debug/net9.0/ERP.Core.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Core Infrastructure layer for Alpac ERP system.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06e9b817fe8c864c1bf1fd5cdf5c12cf64c7577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fec53027a52e3c7f553c079f58105b6f7bee25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
