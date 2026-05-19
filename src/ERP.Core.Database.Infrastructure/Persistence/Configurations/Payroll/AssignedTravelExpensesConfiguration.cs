d6a323cd4541045cefa2fe16a39f5c22a6b41081
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-19 16:26:58 -0600

    feat: guardar tablas de acumulados

--- src/ERP.Core.Database.Infrastructure/Persistence/Configurations/Payroll/AssignedTravelExpensesConfiguration.cs
+++ src/ERP.Core.Database.Infrastructure/Persistence/Configurations/Payroll/AssignedTravelExpensesConfiguration.cs
@@ -1,5 +1,3 @@
-
-
 using ERP.Core.Database.Domain.Entities.Payrolls;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Configurations/Payroll/VacationAccrualConfiguration.cs
@@ -0,0 +1,53 @@
+using ERP.Core.Database.Domain.Entities.Payrolls;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Configurations.Payroll
+{
+    public class VacationAccrualConfiguration : IEntityTypeConfiguration<VacationAccrual>
+    {
+        public void Configure(EntityTypeBuilder<VacationAccrual> builder)
+        {
+            builder.ToTable("vacations");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName("vacation_id")
+                .HasDefaultValueSql("gen_random_uuid()")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(e => e.AvailableVacations)
+                .HasColumnName("available_vacations")
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            builder.Property(e => e.EquivalentQuantity)
+                .HasColumnName("equivalent_quantity")
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            builder.Property(e => e.EquivalentQuantityInDollars)
+                .HasColumnName("equivalent_quantity_in_dollars")
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            builder.Property(e => e.CollaboratorId)
+                .HasColumnName("collaborator_id")
+                .IsRequired();
+
+            builder.Property(e => e.CreatedAt)
+                .HasColumnName("created_at")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.DeletedAt)
+                .HasColumnName("deleted_at");
+                
+            builder.HasIndex(p => p.CollaboratorId)
+                .IsUnique();
+        }
+    }
+}
