3db1d882aace41becce8f35628198aaf05395e5e
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-20 17:08:31 -0600

    chore: guardar cambios

--- src/ERP.Core.Database.Infrastructure/obj/Debug/net9.0/ERP.Core.Database.Infrastructure.AssemblyInfo.cs
+++ src/ERP.Core.Database.Infrastructure/obj/Debug/net9.0/ERP.Core.Database.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.Core.Database.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb3085d6cf9dbccac525c648e6352381d92c6db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324c56c21b8ccaa40295f073af9de59462d82e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Database.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Database.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
