775dcdd6aa1cedcba4fed14eba0eaa156563a773
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-22 20:58:27 -0600

    feat: guardar avances del core

--- src/ERP.Core.Application/obj/Debug/net9.0/ERP.Core.Application.AssemblyInfo.cs
+++ src/ERP.Core.Application/obj/Debug/net9.0/ERP.Core.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Core Application layer for Alpac ERP system.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105e890e1d2fa5518491d291841c7067f00d816b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b677d39b3a70c5a510ebb075420372087b3f049f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ERP.Core.Database.Domain/obj/Debug/net9.0/ERP.Core.Database.Domain.AssemblyInfo.cs
+++ src/ERP.Core.Database.Domain/obj/Debug/net9.0/ERP.Core.Database.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.Core.Database.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105e890e1d2fa5518491d291841c7067f00d816b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b677d39b3a70c5a510ebb075420372087b3f049f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.Core.Database.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.Core.Database.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
