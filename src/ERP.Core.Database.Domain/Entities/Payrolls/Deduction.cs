7c4a3e2a97c0a6c2bd6e3f0da96d0f62fdadaa18
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-05-27 13:18:11 -0600

    feat: guardar cambios

--- src/ERP.Core.Database.Domain/Entities/Payrolls/Deduction.cs
+++ src/ERP.Core.Database.Domain/Entities/Payrolls/Deduction.cs
@@ -17,15 +17,15 @@ namespace ERP.Core.Database.Domain.Entities.Payrolls
         public decimal? FortnightlyAmount { get; set; }          //Monto a deducir quincenalmente.
         public decimal? FortnightlyAmountInDollars { get; set; } //Monto Quincenal a deducir en dolares
 
-        public decimal? Amount { get; set; } // Cantidad Minutos / Otras unidades de medidas
-        public int? Percentage { get; set; } // Esto para embargos, Judiciales y alimenticios
+        public decimal? Amount { get; set; }                // Cantidad Minutos / Otras unidades de medidas
+        public int? Percentage { get; set; }                // Esto para embargos, Judiciales y alimenticios
 
 
         public decimal? TotalBalance { get; set; }          //Saldo restante que tiene el colaborador por pagar.
         public decimal? TotalBalanceInDollars { get; set; } //Saldo restante que tiene el colaborador por pagar.
 
         public decimal? AmountPaid { get; set; }           //Total de dinero pagado en cordobas
-        public decimal? AmountPaidInDollars { get; set; } //Total de dinero pagado en dolares
+        public decimal? AmountPaidInDollars { get; set; }  //Total de dinero pagado en dolares
 
         public decimal TotalAmount { get; set; }          // Monto total de la deducción que se le aplico al colaborador.
         public decimal TotalAmountInDollars { get; set; } // Monto total de la dedución en dolares
