8710af7dfb5962da7451323bb949869d63bf9cce
Desarrollo <desarrollo@jvco.com.ni>
2026-06-22 12:03:03 -0600

    feat:se agg un flag en la entidad collaborator para identificar si es primera vez el registro del colaborador

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260622223151_AgregarIsFirstTimeRegisterCollaborator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarIsFirstTimeRegisterCollaborator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_first_time_register",
+                schema: "public",
+                table: "collaborators",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+            name: "is_first_time_register",
+            schema: "public",
+            table: "collaborators",
+            type: "boolean",
+            nullable: false,
+            defaultValue: true,
+            oldClrType: typeof(bool),
+            oldType: "boolean",
+            oldDefaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_first_time_register",
+                schema: "public",
+                table: "collaborators");
+        }
+    }
+}
--- src/ERP.Core.Database.Infrastructure/Persistence/Repositories/Payroll/CollaboratorsRepository.cs
+++ src/ERP.Core.Database.Infrastructure/Persistence/Repositories/Payroll/CollaboratorsRepository.cs
@@ -5,15 +5,15 @@ using ERP.Core.Database.Application.Commons.Interfaces.Repositories.Payrolls;
 
 namespace ERP.Core.Database.Infrastructure.Persistence.Repositories.Payroll
 {
-    public class CollaboratorsRepository(ErpDbContext _context): Repository<Collaborator>(_context), ICollaboratorsRepository
-    {
-        public async Task<Collaborator> RegisterCollaborator(Collaborator collaborator)
-        {
-            collaborator.Status = CollaboratorStatus.Active;
-            collaborator.PictureUrl = null;
-
-            var collaboratorRegistered = await _context.Collaborators.AddAsync(collaborator);
-            return collaboratorRegistered.Entity;
-        }
-    }
+   public class CollaboratorsRepository(ErpDbContext _context) : Repository<Collaborator>(_context), ICollaboratorsRepository
+   {
+      public async Task<Collaborator> RegisterCollaborator(Collaborator collaborator)
+      {
+         collaborator.Status = CollaboratorStatus.Active;
+         collaborator.PictureUrl = null;
+         collaborator.IsFirstTimeRegister = true;
+         var collaboratorRegistered = await _context.Collaborators.AddAsync(collaborator);
+         return collaboratorRegistered.Entity;
+      }
+   }
 }
