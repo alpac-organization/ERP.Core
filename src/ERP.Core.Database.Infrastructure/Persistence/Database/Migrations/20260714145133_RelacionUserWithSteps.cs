50164e5bf83472becd75b2f35cb2d3e1ee4760ed
David Cortes <davidcortes0209@gmail.com>
2026-07-14 08:52:23 -0600

    Chore: Agregar migracion de relacion User con Step

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260714145133_RelacionUserWithSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionUserWithSteps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(450)",
+                oldMaxLength: 450);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_step_execution_logs_managua_processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua",
+                column: "processed_by_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_step_execution_logs_managua_users_processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua",
+                column: "processed_by_user_id",
+                principalSchema: "public",
+                principalTable: "users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_step_execution_logs_managua_users_processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua");
+
+            migrationBuilder.DropIndex(
+                name: "IX_step_execution_logs_managua_processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "processed_by_user_id",
+                schema: "public",
+                table: "step_execution_logs_managua",
+                type: "character varying(450)",
+                maxLength: 450,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
