4764b7ed64e2f04d2b6c2f56976df19715d18cce
David Cortes <davidcortes0209@gmail.com>
2026-07-22 15:34:09 -0600

    feat(#168): closed: #168 - migracion para los nuevos campos

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260722213219_AdicionCamposAuditables.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionCamposAuditables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "processed_by_user_name",
+                schema: "public",
+                table: "step_execution_logs",
+                type: "character varying(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "medio_exit_date",
+                schema: "public",
+                table: "reception_entrance",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "medio_exit_time",
+                schema: "public",
+                table: "reception_entrance",
+                type: "time without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "updated_by_user_name",
+                schema: "public",
+                table: "reception_entrance",
+                type: "character varying(450)",
+                maxLength: 450,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "processed_by_user_name",
+                schema: "public",
+                table: "step_execution_logs");
+
+            migrationBuilder.DropColumn(
+                name: "medio_exit_date",
+                schema: "public",
+                table: "reception_entrance");
+
+            migrationBuilder.DropColumn(
+                name: "medio_exit_time",
+                schema: "public",
+                table: "reception_entrance");
+
+            migrationBuilder.DropColumn(
+                name: "updated_by_user_name",
+                schema: "public",
+                table: "reception_entrance");
+        }
+    }
+}
