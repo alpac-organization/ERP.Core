048b4f15484c7389cc7eefe3fdb60505ba2f40cc
David Cortes <davidcortes0209@gmail.com>
2026-08-11 15:53:49 -0600

    cambios de campos a null

--- src/ERP.Core.Database.Domain/Entities/Warehouse/WarehouseCapacity.cs
+++ src/ERP.Core.Database.Domain/Entities/Warehouse/WarehouseCapacity.cs
@@ -7,13 +7,13 @@ public class WarehouseCapacity : BaseEntity<Guid>
     public Guid WarehouseId { get; set; }
 
     public decimal TotalAreaM2 { get; set; }
-    public decimal UsableAreaM2 { get; set; }
-    public decimal UnusableAreaM2 { get; set; }
+    public decimal? UsableAreaM2 { get; set; }
+    public decimal? UnusableAreaM2 { get; set; }
 
-    public int TotalMaxPolines { get; set; }
-    public int CurrentPolinesStored { get; set; }
+    public int? TotalMaxPolines { get; set; }
+    public int? CurrentPolinesStored { get; set; }
 
-    public DateTime LastCalculatedAt { get; set; }
+    public DateTime? LastCalculatedAt { get; set; }
 
     public virtual Warehouses Warehouse { get; set; } = null!;
 }
