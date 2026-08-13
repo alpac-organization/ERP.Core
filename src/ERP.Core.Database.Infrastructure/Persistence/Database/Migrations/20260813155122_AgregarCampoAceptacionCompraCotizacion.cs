45e0b560408d92cfea991110eedccd4f76a0d42f
Angel-Delg <lariosangel062@gmail.com>
2026-08-13 10:09:52 -0600

    feat(#255): resolved: #255 - Agregar bandera para poder identificar que cotización fue seleccionada para su compra

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260813155122_AgregarCampoAceptacionCompraCotizacion.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCampoAceptacionCompraCotizacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_aceepted_for_purchase",
+                schema: "public",
+                table: "quotations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_aceepted_for_purchase",
+                schema: "public",
+                table: "quotations");
+        }
+    }
+}
