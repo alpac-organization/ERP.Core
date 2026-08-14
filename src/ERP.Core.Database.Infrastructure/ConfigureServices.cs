e48cdc2ad991e31c71d8f8dad15c73648154b4dc
Donald Jose Munguia Guadamuz <jdonaldmunguia@gmail.com>
2026-08-14 10:00:55 -0600

    fix: se agrega management review status enum a configure service

--- src/ERP.Core.Database.Infrastructure/ConfigureServices.cs
+++ src/ERP.Core.Database.Infrastructure/ConfigureServices.cs
@@ -76,6 +76,7 @@ namespace ERP.Core.Database.Infrastructure
                     npgsqlOptions.MapEnum<SectionType>("section_type_enum", "public");
                     npgsqlOptions.MapEnum<SectionStorageType>("section_storage_type_enum", "public");
                     npgsqlOptions.MapEnum<AccountingReviewStatus>("accounting_review_status_enum", "public");
+                    npgsqlOptions.MapEnum<ManagementReviewStatus>("management_review_status_enum", "public");
                     npgsqlOptions.MapEnum<RackUsageProfile>("rack_usage_profile_enum", "public");
                     npgsqlOptions.MapEnum<RackStatus>("rack_status_enum", "public");
                 })
