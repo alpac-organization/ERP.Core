62d19fdfdeafb912004e8c17a50a2a75daa20f91
David Cortes <davidcortes0209@gmail.com>
2026-09-09 16:54:40 -0600

    entidades corregidas

--- src/ERP.Core.Database.Domain/Entities/Bases/BaseCapacity.cs
+++ src/ERP.Core.Database.Domain/Entities/Bases/BaseCapacity.cs
@@ -2,18 +2,17 @@ namespace ERP.Core.Database.Domain.Entities.Bases
 {
     public abstract class BaseCapacity : BaseEntity<Guid>
     {
-        public decimal Witdh { get; set; }
+        public decimal Width { get; set; }
         public decimal Length { get; set; }
 
         //M2
         public decimal UnusedAreaM2 { get; set; }  //area inutil
-        public decimal AvailableAreaWithSpacingM2 { get; set; }  //area util
+        public decimal AvailableAreaWithMarginM2 { get; set; }  //area util
         public decimal TotalAreaM2 { get; set; } //Area Total = area util + area inutil = Witdh * Length
 
-        public decimal VacantChargeableAreaM2 { get; set; } //Area Disponible para Cobro
-        public decimal OccupiedChargeableAreaM2 { get; set; } //Area Ocupada para Cobrar
+        public decimal UnoccupiedChargeableAreaM2 { get; set; } //Area facturable desocupada
+        public decimal OccupiedChargeableAreaM2 { get; set; }   //Area facturable ocupada
 
-        public decimal PercenteAvailableAreaWithSpacingM2 { get; set; }
-        public decimal PercenteAvailableVolumenWithSpacingM3 { get; set; }
+        public decimal PercentageAvailableAreaWithMarginM2 { get; set; }
     }
 }
--- src/ERP.Core.Database.Domain/Entities/Catalogs/SectionCapacity.cs
+++ src/ERP.Core.Database.Domain/Entities/Catalogs/SectionCapacity.cs
@@ -3,10 +3,7 @@ using ERP.Core.Database.Domain.Entities.Bases;
 namespace ERP.Core.Database.Domain.Entities.Catalogs
 {
     public class SectionCapacity : BaseCapacity
-    {
-        public decimal? UsableAreaM2 { get; set; }
-        public decimal? UnusableAreaM2 { get; set; }
-        
+    {        
         public Guid SectionId { get; set; }
         public virtual Sections Section { get; set; } = null!;
     }
