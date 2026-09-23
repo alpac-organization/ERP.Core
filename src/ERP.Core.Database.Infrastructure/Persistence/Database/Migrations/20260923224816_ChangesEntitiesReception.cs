efa9f8549ceccf888e88f17df89d50d1412e1933
David Cortes <davidcortes0209@gmail.com>
2026-09-23 16:48:37 -0600

    new migration

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260923224816_ChangesEntitiesReception.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesEntitiesReception : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "duca_number",
+                schema: "public",
+                table: "operational_orders",
+                newName: "document_number");
+
+            migrationBuilder.AddColumn<int>(
+                name: "packages_count",
+                schema: "public",
+                table: "operational_orders",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "weight",
+                schema: "public",
+                table: "operational_orders",
+                type: "numeric(8,2)",
+                precision: 8,
+                scale: 2,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "packages_count",
+                schema: "public",
+                table: "operational_orders");
+
+            migrationBuilder.DropColumn(
+                name: "weight",
+                schema: "public",
+                table: "operational_orders");
+
+            migrationBuilder.RenameColumn(
+                name: "document_number",
+                schema: "public",
+                table: "operational_orders",
+                newName: "duca_number");
+        }
+    }
+}
