8c94794b288b8ab38f56189815e6623c926142b1
David Cortes <davidcortes0209@gmail.com>
2026-09-24 17:09:31 -0600

    quitar restriccion unico de code en OP

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260924230900_DontUniqueCodeOP.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class DontUniqueCodeOP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_operational_orders_op_code",
+                schema: "public",
+                table: "operational_orders");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_operational_orders_op_code",
+                schema: "public",
+                table: "operational_orders",
+                column: "op_code");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_operational_orders_op_code",
+                schema: "public",
+                table: "operational_orders");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_operational_orders_op_code",
+                schema: "public",
+                table: "operational_orders",
+                column: "op_code",
+                unique: true);
+        }
+    }
+}
