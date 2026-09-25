db08d69b4ef48475a4dc50d61ca302372a2272ef
David Cortes <davidcortes0209@gmail.com>
2026-09-24 22:03:02 -0600

    migracon

---
+++ src/ERP.Core.Database.Infrastructure/Persistence/Database/Migrations/20260925040212_ChangeTypeYearMachinery.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Core.Database.Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeYearMachinery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Year",
+                schema: "public",
+                table: "machineries",
+                newName: "year");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "year",
+                schema: "public",
+                table: "machineries",
+                type: "character varying(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "year",
+                schema: "public",
+                table: "machineries",
+                newName: "Year");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                schema: "public",
+                table: "machineries",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(4)",
+                oldMaxLength: 4);
+        }
+    }
+}
